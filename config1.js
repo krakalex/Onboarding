e9c6a17cd0a6a5750774f10d42d4869f76b2554e
Aliaksandr Krakasevich <alexsashakr@gmail.com>
2024-08-23 20:43:36 +0300

    trial

---
+++ config1.js
@@ -0,0 +1,55 @@
+const QmateService = require("@sap_oss/wdio-qmate-service");
+
+exports.config = {
+
+    baseUrl: "https://sapui5.hana.ondemand.com/test-resources/sap/m/demokit/cart/webapp/index.html",
+    
+    specs: [
+      [
+         "./specs/01_createNewPO.spec.js"
+
+      ],
+    ],
+
+    framework: 'mocha',
+    mochaOpts: {
+      timeout: 40000,
+      bail: true
+    },
+  services: [[QmateService], ['chromedriver']],
+  
+  params: {
+    import: {
+      data: "./data/",
+      orderConfirmation: "./data/orderConfirmation.json"
+    },
+    export: {
+      orderConfirmation: "./data/orderConfirmation.json"
+    }
+  },
+  
+  capabilities: [{
+      browserName: "chrome",
+      acceptInsecureCerts: true,
+      'goog:chromeOptions': {
+        args: [
+            "--output-/dev/null",
+            "--log-level-3",
+            "--no-sandbox",
+            "--disable-search-engine-choice-screen",
+           // "--incognito",
+            "--ignore-certificate-errors",
+            "--window-size-1920,1200",
+            "--unftelisted-ios",
+            "--disable-dev-shm-usage",
+            // "--headless",
+            "--disable-gpu",
+            "--disable-web-security",
+            "--disable-infobars",
+            "--disable-extensions",
+            "--disable-logging",
+            "--lang-en-US"
+      ]
+    }
+  }]
+};
---
+++ specs/01_createNewPO.spec.js
@@ -0,0 +1,149 @@
+describe("Place a new Order", function(){
+
+    it("Step 01: Open the Fiori app", async function(){
+        await ui5.navigation.navigateToApplication("");
+        // await util.browser.sleep(5000);
+    });
+
+    it("Step 02: Click Add to Shopping Cart", async function(){
+        const selector = {
+            "elementProperties": {
+                "viewName": "sap.ui.demo.cart.view.Welcome",
+                "metadata": "sap.ui.core.Icon",
+                "bindingContextPath": "/Promoted/0"
+            }
+        };
+        await ui5.userInteraction.click(selector);
+    });
+    it("Step 03: Click Show Shopping Cart", async function(){
+        const selector = {
+            "elementProperties": {
+                "viewName": "sap.ui.demo.cart.view.Welcome",
+                "metadata": "sap.ui.core.Icon",
+                "src": "sap-icon://cart"
+            }
+        };
+        await ui5.userInteraction.click(selector);
+    });
+    it("Step 04: Click Process button", async function(){
+        const selector = {
+            "elementProperties": {
+                "viewName": "sap.ui.demo.cart.view.Cart",
+                "metadata": "sap.m.Button",
+                "id": "*proceedButton"
+            }
+        };
+        await ui5.userInteraction.click(selector);
+    });
+    it("Step 05: Click Step 2 button", async function(){
+        const selector = {
+            "elementProperties": {
+                "viewName": "sap.ui.demo.cart.view.Checkout",
+                "metadata": "sap.m.Button",
+                "id": "*contentsStep-nextButton"
+            }
+        };
+        await ui5.userInteraction.click(selector);
+    });
+    it("Step 06: Choose Bank transfer", async function(){
+        const selector = {
+            "elementProperties": {
+                "viewName": "sap.ui.demo.cart.view.Checkout",
+                "metadata": "sap.m.Button",
+                "id": "*payViaBank-button"
+            }
+        };
+        await ui5.userInteraction.click(selector);
+    });
+    it("Step 07: Proceed to Step 3", async function(){
+        const selector = {
+            "elementProperties": {
+                "viewName": "sap.ui.demo.cart.view.Checkout",
+                "metadata": "sap.m.Button",
+                "id": "*paymentTypeStep-nextButton"
+            }
+        };
+        await ui5.userInteraction.click(selector);
+    })
+    it("Step 07: Proceed to Step 4", async function(){
+        const selector = {
+            "elementProperties": {
+                "viewName": "sap.ui.demo.cart.view.Checkout",
+                "metadata": "sap.m.Button",
+                "id": "*bankAccountStep-nextButton"
+            }
+        };
+        await ui5.userInteraction.click(selector);
+    })
+    it("Step 08: Input address", async function(){
+        const selector = {
+            "elementProperties": {
+                "viewName": "sap.ui.demo.cart.view.Checkout",
+                "metadata": "sap.m.Input",
+                "id": "*invoiceAddressAddress"
+            }
+        };
+        await ui5.userInteraction.click(selector);
+        await ui5.userInteraction.clearAndFill(selector, "My Value");
+        await common.userInteraction.pressTab();
+        await common.userInteraction.clearAndFillActive("My Value");
+        await common.userInteraction.pressTab();
+        await common.userInteraction.clearAndFillActive("220022");
+        await common.userInteraction.pressTab();
+        await common.userInteraction.clearAndFillActive("Neverland");
+        await common.userInteraction.pressEnter();
+    })
+    it("Step 09: Proceed to Step 5", async function(){
+        const selector = {
+            "elementProperties": {
+                "viewName": "sap.ui.demo.cart.view.Checkout",
+                "metadata": "sap.m.Button",
+                "id": "*invoiceStep-nextButton"
+            }
+        };
+        await ui5.userInteraction.click(selector);
+    })
+    it("Step 10: Proceed to the Order Summary", async function(){
+        const selector = {
+            "elementProperties": {
+                "viewName": "sap.ui.demo.cart.view.Checkout",
+                "metadata": "sap.m.Button",
+                "id": "*deliveryTypeStep-nextButton"
+            }
+        };
+        await ui5.userInteraction.click(selector);
+    })
+    it("Step 11: Submit the Order", async function(){
+        const selector = {
+            "elementProperties": {
+                "viewName": "sap.ui.demo.cart.view.Checkout",
+                "metadata": "sap.m.Button",
+                "id": "*submitOrder"
+            }
+        };
+        await ui5.userInteraction.click(selector);
+        await util.browser.sleep(1000);
+        await common.userInteraction.pressEnter();
+        // await common.userInteraction.pressEnter();
+        // await common.userInteraction.pressEnter();
+        await util.browser.sleep(5000);
+    })
+    it("Step 12: Get newly created Order ID", async function(){
+        const selector = {
+            "elementProperties": {
+                "viewName": "sap.ui.demo.cart.view.OrderCompleted",
+                "metadata": "sap.m.FormattedText"
+            }
+        };
+        const OrderID = await ui5.element.getPropertyValue(selector, "htmlText");
+        util.console.log(OrderID);
+        const userData = {
+            "orderConfirmation": OrderID
+        };
+        browser.config.params.export.orderConfirmation = userData;
+
+        const references = browser.config.params.import.data["references"];
+        references.orderConfirmation = OrderID;
+        
+    })
+})
